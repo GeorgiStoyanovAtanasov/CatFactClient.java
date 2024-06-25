5d50f873b61e5f9cd0e4e9193ce0cade5e750d3e
first-branch <radeva7609@gmaill.com>
2024-06-25 09:10:15 +0300

    Initial commit

---
+++ src/main/java/com/example/consuming_rest_apis/CatFact.java
@@ -0,0 +1,48 @@
+package com.example.consuming_rest_apis;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+@Entity
+public class CatFact {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String fact;
+    private int length;
+
+    public CatFact() {
+    }
+
+    public CatFact(String fact, int length) {
+        this.fact = fact;
+        this.length = length;
+    }
+
+    public String getFact() {
+        return fact;
+    }
+
+    public void setFact(String fact) {
+        this.fact = fact;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
